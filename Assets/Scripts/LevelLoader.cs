636228d9d5059e9d940d5dabce1f609b63893cbf
ijuratovac <ijuratovac@vub.hr>
2023-02-03 18:46:24 +0100

    new map A3, swapped with A2

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class LevelLoader : MonoBehaviour {
 
     public Slider slider;
+    public GameObject loadingScene;
 
     public void LoadLevel(int sceneIndex) {
         StartCoroutine(LoadAsynchronously(sceneIndex));
@@ -13,6 +14,8 @@ public class LevelLoader : MonoBehaviour {
 
     IEnumerator LoadAsynchronously(int sceneIndex) {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScene.SetActive(true);
 
         while (!operation.isDone) {
             float progress = Mathf.Clamp01(operation.progress / 0.9f);
